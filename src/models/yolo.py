4f90e39a7f90d9df414c346e0505df1ddbec80cf
JustCati <ale.catinello@gmail.com>
2024-10-12 19:13:02 +0200

    feat: get yolo checkpoint

--- src/models/yolo.py
+++ src/models/yolo.py
@@ -1,2 +1,12 @@
-from ultralytics import YOLOV10
+import os
+import torch
+from ultralytics import YOLOv10
 
+
+
+def getYOLO(checkpoint_path: str, device: str = 'cpu') -> YOLOv10:
+    if not os.path.exists(checkpoint_path):
+        raise ValueError('Checkpoint path does not exist')
+    model = YOLOv10(checkpoint_path)
+    model.to(device)
+    return model
