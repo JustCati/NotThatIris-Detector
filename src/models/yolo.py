0fa5c6a0f0b19d56083d9fbaaa02c464ea93faa1
JustCati <ale.catinello@gmail.com>
2024-10-14 08:56:15 +0200

    fix: YOLO class instead of YOLOv10

--- src/models/yolo.py
+++ src/models/yolo.py
@@ -1,12 +1,12 @@
 import os
 import torch
-from ultralytics import YOLOv10
+from ultralytics import YOLO
 
 
 
-def getYOLO(checkpoint_path: str, device: str = 'cpu') -> YOLOv10:
+def getYOLO(checkpoint_path: str, device: str = 'cpu') -> YOLO:
     if not os.path.exists(checkpoint_path):
         raise ValueError('Checkpoint path does not exist')
-    model = YOLOv10(checkpoint_path)
+    model = YOLO(checkpoint_path)
     model.to(device)
     return model
