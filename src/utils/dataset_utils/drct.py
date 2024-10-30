18616121a596d15a6b58beeb5ae34c74ab35f640
JustCati <ale.catinello@gmail.com>
2024-10-30 16:48:12 +0100

    feat[image copy]: file counting

--- src/utils/dataset_utils/drct.py
+++ src/utils/dataset_utils/drct.py
@@ -14,13 +14,13 @@ def create_dataset(src_path, dst_path):
 
     src_file_count = sum([len(files) for _, _, files in os.walk(src_path)])
     dst_file_count = sum([len(files) for _, _, files in os.walk(dst_path)])
-    print(src_file_count, dst_file_count)
 
     if src_file_count != dst_file_count:
         shutil.rmtree(dst_path)
         os.makedirs(dst_path)
         all_files = [os.path.join(root, file) for root, _, files in os.walk(src_path) for file in files]
-        for file in tqdm(all_files):
-            dst_file_path = os.path.join(dst_path, os.path.basename(file))
-
-            shutil.copy(file, dst_file_path)
+        for idx, file in enumerate(tqdm(all_files)):
+            filename, file_ext = os.path.splitext(file)
+            dst_file_path = filename + "_" + str(idx) + file_ext
+            shutil.copy(file, os.path.join(dst_path, os.path.basename(dst_file_path)))
+    return dst_path
