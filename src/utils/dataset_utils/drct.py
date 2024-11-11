efa7cff609d504864c361a1a960e10a4e144a85b
JustCati <ale.catinello@gmail.com>
2024-11-11 12:35:29 +0100

    feat: parallelize i/o operations

--- src/utils/dataset_utils/drct.py
+++ src/utils/dataset_utils/drct.py
@@ -2,9 +2,17 @@ import os
 import shutil
 import random
 from PIL import Image
-from tqdm import tqdm
+import multiprocessing
+from pqdm.threads import pqdm
 
 
+def resize_image(file, dst_path, scale_factor):
+    img = Image.open(file)
+    h, w = img.size
+    new_h, new_w = h // scale_factor, w // scale_factor
+
+    img = img.resize((new_h, new_w), Image.BICUBIC)
+    img.save(os.path.join(dst_path, os.path.basename(file)))
 
 
 def generate_lq_images(src_path, DOWN_SCALE_FACTOR=4):
@@ -23,14 +31,22 @@ def generate_lq_images(src_path, DOWN_SCALE_FACTOR=4):
         shutil.rmtree(dst_path)
         os.makedirs(dst_path)
 
+        cpu_count = multiprocessing.cpu_count()
         all_files = [os.path.join(root, file) for root, _, files in os.walk(src_path) for file in files]
-        for file in tqdm(all_files):
-            img = Image.open(file)
-            h, w = img.size
-            new_h, new_w = h // DOWN_SCALE_FACTOR, w // DOWN_SCALE_FACTOR
+        args = [{
+            "file": file,
+            "dst_path": dst_path,
+            "scale_factor": DOWN_SCALE_FACTOR} for file in all_files]
+        pqdm(args, resize_image, n_jobs=cpu_count, argument_type="kwargs")
 
-            img = img.resize((new_h, new_w), Image.BICUBIC)
-            img.save(os.path.join(dst_path, os.path.basename(file)))
+
+def split_single_image(idx, file, train_hq_path, val_hq_path, train_lq_path, val_lq_path, train_idx):
+    if idx in train_idx:
+        shutil.copy(file, os.path.join(train_hq_path, os.path.basename(file)))
+        shutil.copy(file.replace('hq', 'lq'), os.path.join(train_lq_path, os.path.basename(file)))
+    else:
+        shutil.copy(file, os.path.join(val_hq_path, os.path.basename(file)))
+        shutil.copy(file.replace('hq', 'lq'), os.path.join(val_lq_path, os.path.basename(file)))
 
 
 def split_dataset(src_path, dst_path, split_ratio=0.8):
@@ -64,21 +80,25 @@ def split_dataset(src_path, dst_path, split_ratio=0.8):
     random.shuffle(indexis)
     train_idx = set(indexis[:train_count])
 
+    cpu_count = multiprocessing.cpu_count()
     all_files = [os.path.join(root, file) for root, _, files in os.walk(src_path) for file in files]
-    for idx, file in enumerate(tqdm(all_files)):
-        if idx in train_idx:
-            shutil.copy(file, os.path.join(train_hq_path, os.path.basename(file)))
-            shutil.copy(file.replace('hq', 'lq'), os.path.join(train_lq_path, os.path.basename(file)))
-        else:
-            shutil.copy(file, os.path.join(val_hq_path, os.path.basename(file)))
-            shutil.copy(file.replace('hq', 'lq'), os.path.join(val_lq_path, os.path.basename(file)))
+    args = [{"idx": idx, "file": file, 
+             "train_hq_path": train_hq_path,
+             "val_hq_path": val_hq_path,
+             "train_lq_path": train_lq_path,
+             "val_lq_path": val_lq_path,
+             "train_idx": train_idx}  for idx, file in enumerate(all_files)]
+    pqdm(args, split_single_image, n_jobs=cpu_count, argument_type="kwargs")
 
 
 
+def copy_image(idx, file, dst_path):
+    filename, file_ext = os.path.splitext(file)
+    dst_file_path = filename + "_" + str(idx) + file_ext
+    shutil.copy(file, os.path.join(dst_path, os.path.basename(dst_file_path)))
 
 
-
-def create_dataset(src_path, dst_path, train_split_ratio=0.8):
+def create_dataset(src_path, dst_path, scaling_factor, train_split_ratio=0.8):
     dst_path = os.path.join(dst_path, 'hq')
     if not os.path.exists(dst_path):
         os.makedirs(dst_path)
@@ -90,15 +110,19 @@ def create_dataset(src_path, dst_path, train_split_ratio=0.8):
         print(f"Found {dst_file_count} images in the destination instead of {src_file_count}, copying images to remove people tagging...") 
         shutil.rmtree(dst_path)
         os.makedirs(dst_path)
+
+        cpu_count = multiprocessing.cpu_count()
         all_files = [os.path.join(root, file) for root, _, files in os.walk(src_path) for file in files]
-        for idx, file in enumerate(tqdm(all_files)):
-            filename, file_ext = os.path.splitext(file)
-            dst_file_path = filename + "_" + str(idx) + file_ext
-            shutil.copy(file, os.path.join(dst_path, os.path.basename(dst_file_path)))
+        args = [{"idx": idx, 
+                 "file": file,
+                 "dst_path": dst_path} for idx, file in enumerate(all_files)]
+        pqdm(args, copy_image, n_jobs=cpu_count, argument_type="kwargs")
+
         for file in os.listdir(dst_path):
             if file.startswith("Thumbs"):
                 os.remove(os.path.join(dst_path, file))
 
+    generate_lq_images(dst_path, scaling_factor)
 
     train_path = os.path.join(os.path.dirname(dst_path), 'train')
     dst_file_count = sum([len(files) for _, _, files in os.walk(dst_path)])
@@ -107,4 +131,3 @@ def create_dataset(src_path, dst_path, train_split_ratio=0.8):
     if train_count != (train_split_ratio * dst_file_count):
         print("Splitting dataset")
         split_dataset(dst_path, os.path.dirname(dst_path), train_split_ratio)
-    return dst_path
