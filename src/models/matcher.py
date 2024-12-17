c5c071c5b002e14b0fe6ae60dc9f74e9e5cb92e7
JustCati <ale.catinello@gmail.com>
2024-12-17 12:04:14 +0100

    feat: VectorStore for gallery and retrieval

--- src/models/matcher.py
+++ src/models/matcher.py
@@ -24,3 +24,36 @@ class GalleryDB(Chroma):
 
 
 
+class VectorStore():
+    def __init__(self, model_path, collection_name, out_path="", device="cpu"):
+        self.device = device
+        self.model = FeatureExtractor(Resnet.load_from_checkpoint(model_path, num_classes=819)).to(device)
+        self.vector_store = GalleryDB(collection_name=collection_name, persist_directory=out_path)
+
+
+    def generate_embedding(self, imgs):
+        if not isinstance(imgs, list):
+            imgs = [imgs]
+        imgs = [transforms.ToTensor()(img) for img in imgs if isinstance(img, Image.Image)]
+
+        with torch.no_grad():
+            imgs = torch.stack(imgs).to(self.device)
+            embeddings = self.model(imgs)
+        final_embedding = torch.mean(embeddings, dim=0)
+        return final_embedding.cpu().numpy()
+
+
+    def query(self, img):
+        img = Image.open(img)
+        embedding = self.generate_embedding(img)
+        toRet = self.vector_store.similarity_search_by_vector_with_relevance_scores(embedding, 1)
+        id = toRet[0][0].page_content
+        similarity = 1 - toRet[0][1]
+        return id, similarity
+
+
+    def add_user(self, imgs, label):
+        self.vector_store.add_embedding(self.generate_embedding(imgs if isinstance(imgs, list) else [imgs]),
+                                        label,
+                                        label if isinstance(label, str) else str(label))
+
