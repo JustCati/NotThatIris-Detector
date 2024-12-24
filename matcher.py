0836679203816e99987eedb37d576bfb0672869e
JustCati <ale.catinello@gmail.com>
2024-12-24 16:31:33 +0100

    feat: add support for MLPMatcher and update evaluation functions based on matcher type

--- matcher.py
+++ matcher.py
@@ -1,27 +1,23 @@
 import os
 import torch
 import argparse
+import pandas as pd
 from tqdm import tqdm
+import multiprocessing
 from torch.utils.data import DataLoader
 
 from src.models.adapter import FeatureAdapter
 from src.models.resnet import FeatureExtractor
+from src.models.mlp_matcher import MLPMatcher
 from src.models.gallery_classifier import Matcher
 from external.DRCT.drct.archs.DRCT_arch import DRCT
-from src.engine.thresholding import evaluate, get_eer
 from src.graphics.metrics import roc_graph, far_frr_graph
 from src.dataset.GenericIrisDataset import GenericIrisDataset
 from src.models.GenericFeatureExtractor import GenericFeatureExtractor
+from src.engine.thresholding import evaluate_vectorbased, get_eer, evaluate_mlp
 from src.utils.dataset_utils.upsample import generate_upsampled_normalized_iris
 from src.utils.dataset_utils.iris import normalize_iris_thousand, split_iris_thousand_users
 
-
-
-def get_label_map(csv_file):
-    df = pd.read_csv(csv_file, index_col=0)
-    label_map = {label: i for i, label in enumerate(df["Label"].unique())}
-    label_map.update({"-1": -1})
-    return label_map
 
 
 def get_label_map(csv_file):
@@ -57,6 +53,8 @@ def main(args):
     eval_csv_path = os.path.join(dataset_path, "val_users.csv")
     complete_csv_path = os.path.join(dataset_path, "iris_thousands.csv")
 
+
+    #* -------- CHECK IF THE DATASET IS PREPROCESSED -----------
     if not os.path.exists(images_path):
         raise FileNotFoundError(f"Images path not found: {images_path}")
     if not os.path.exists(train_csv_path) or not os.path.exists(test_csv_path) or not os.path.exists(eval_csv_path):
@@ -113,35 +111,53 @@ def main(args):
         else:
             print("Adapter model not found, using the feature extractor only...")
 
-    matcher = Matcher(model=feat_extractor, 
-                      collection_name=collection_name,
-                      out_path=persistent_outpath,
-                      device=device)
+
+    #* ------------- LOAD MATCHER -------------
+    if args.type == "vector":
+        matcher = Matcher(model=feat_extractor, 
+                        collection_name=collection_name,
+                        out_path=persistent_outpath,
+                        device=device)
+    elif args.type == "mlp":
+        mlp_model_path = args.mlp_model_path
+        matcher = MLPMatcher.load_from_checkpoint(mlp_model_path,
+                                                  in_feature=2048,
+                                                  num_classes=1597,
+                                                  extractor=feat_extractor).to(device)
+        matcher.eval()
 
 
-    #* LOAD USERS INTO MATCHER
-    print("Loading users into matcher...")
-    for imgs, labels in tqdm(train_dataloader):
-        matcher.add_user(imgs, labels)
-    print()
+    #* --------- LOAD USERS INTO MATCHER (ONLY FOR VECTOR-BASED MATCHER) ---------
+    if args.type == "vector":
+        print("Loading users into matcher...")
+        for imgs, labels in tqdm(train_dataloader):
+            matcher.add_user(imgs, labels)
+        print()
 
 
-    #* FINE TUNE THE THRESHOLD OF THE MATCHER
+    #* ---------- FINE TUNE THE THRESHOLD OF THE MATCHER ----------
     print("Fine tuning the threshold of the matcher...")
-    y, y_pred = evaluate(matcher, test_dataloader, train=True)
+    if args.type == "vector":
+        y, y_pred = evaluate_vectorbased(matcher, test_dataloader, train=True)
+    else:
+        y, y_pred = evaluate_mlp(matcher, test_dataloader)
     far, frr, tpr, threshold, eer_index, eer_threshold = get_eer(y, y_pred)
     print(f"FAR: {far[eer_index]:.4f}, FRR: {frr[eer_index]:.4f}, Threshold at EER: {eer_threshold:.4f}")
     print()
 
 
-    #* EVALUATE MATCHER
+
+    #* ----------- EVALUATE MATCHER -----------
     print("Evaluating the matcher...")
-    matcher.set_threshold(eer_threshold) #? Set the threshold to the EER threshold
-    y, y_pred = evaluate(matcher, eval_dataloader)
+    matcher.set_threshold(eer_threshold)
+    if args.type == "vector":
+        y, y_pred = evaluate_vectorbased(matcher, eval_dataloader)
+    else:
+        y, y_pred = evaluate_mlp(matcher, eval_dataloader)
     print()
 
     eval_far, eval_frr, _, _, _, _ = get_eer(y, y_pred)
-    print(f"FAR: {eval_far[eer_index]:.4f}, FRR: {eval_frr[eer_index]:.4f}, Threshold at EER: {eer_threshold:.4f}")
+    print(f"FAR: {eval_far[eer_index]:.4f}, FRR: {eval_frr[eer_index]:.4f}, Threshold Used: {eer_threshold:.4f}")
     print()
 
 
@@ -154,9 +170,10 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Matcher")
-    parser.add_argument("type", type=str, help="Type of the matcher", choices=["vector", "mlp", "ensemble"])
+    parser.add_argument("type", type=str, help="Type of the matcher", choices=["vector", "mlp"])
     parser.add_argument("--resnet_model_path", type=str, required=True, help="Path to the feature extraction model")
     parser.add_argument("--adapter_model_path", type=str, default=None, help="Path to the adapter model")
+    parser.add_argument("--mlp_model_path", type=str, default=None, help="Path to the MLP model")
     parser.add_argument("--sr_model_path", type=str, help="Path to the super resolution model")
     parser.add_argument("--plot", action="store_true", help="Whether to plot the graphs")
     parser.add_argument("--upsample", action="store_true", help="Whether to use Super Resolution on the images")
