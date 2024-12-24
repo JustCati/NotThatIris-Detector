f02addc5719faf2d2fb26bc84bc3efecf64d7410
JustCati <ale.catinello@gmail.com>
2024-12-24 12:50:16 +0100

    feat: mean loss during training and logging at evaluation epoch end

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -14,8 +14,13 @@ warnings.filterwarnings("ignore")
 class Resnet(pl.LightningModule):
     def __init__(self, batch_size=32, num_classes=2000, verbose = False):
         super().__init__()
-        self.y = []
-        self.y_pred = []
+        self._y = []
+        self._y_pred = []
+        self._y_loss = []
+        self._y_hat_loss = []
+        self._training_loss = []
+        self._loss_value = 1_000_000.0 # Just a big number
+
         self.batch_size = batch_size
         self.model = resnet50(ResNet50_Weights.IMAGENET1K_V2)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
@@ -33,7 +38,7 @@ class Resnet(pl.LightningModule):
         y_hat = self(x.to(self.device))
 
         loss = nn.CrossEntropyLoss()(y_hat, y)
-        self.log("train/train_loss", loss)
+        self._training_loss.append(loss.item())
         return loss
 
 
@@ -41,22 +46,37 @@ class Resnet(pl.LightningModule):
         x, y = batch
         y_hat = self(x.to(self.device))
 
-        y_for_loss = y.view(-1).long()
-        loss = nn.CrossEntropyLoss()(y_hat, y_for_loss)
-        self.log("eval/val_loss", loss)
+        y_loss = y.view(-1).long()
+        self._y_loss.extend(y_loss.cpu().numpy())
+        self._y_hat_loss.extend(y_hat.cpu().numpy())
 
         y = y.cpu().numpy()
         y_hat = y_hat.argmax(dim=1).cpu().numpy()
-        self.y.extend(y)
-        self.y_pred.extend(y_hat)
+        self._y.extend(y)
+        self._y_pred.extend(y_hat)
+
+
+    def on_train_epoch_end(self):
+        loss = torch.tensor(self._training_loss).mean()
+        self._loss_value = loss
+        self._training_loss = []
 
 
     def on_validation_epoch_end(self):
-        f1 = f1_score(self.y, self.y_pred, average="macro")
+        y_loss = torch.tensor(self._y_loss)
+        y_hat_loss = torch.tensor(self._y_hat_loss)
+
+        loss = nn.CrossEntropyLoss()(y_hat_loss, y_loss)
+        self.log("eval/val_loss", loss)
+        self.log("train/loss", self._loss_value)
+
+        f1 = f1_score(self._y, self._y_pred, average="macro")
         self.log("eval/f1", f1)
-        
-        self.y = []
-        self.y_pred = []
+
+        self._y = []
+        self._y_pred = []
+        self._y_loss = []
+        self._y_hat_loss = []
 
 
     def test_step(self, batch, batch_idx):
