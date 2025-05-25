e0e5cca1467c39b4561112a5861cb85f1b44b375
JustCati <ale.catinello@gmail.com>
2025-05-25 12:05:18 +0200

    refactor: enhance eye normalization process by using YOLOSEG to detect iris and pupil

--- src/utils/eyes.py
+++ src/utils/eyes.py
@@ -31,72 +31,86 @@ def daugman_normalization(image, height, width, r_in, r_out):
     return flat
 
 
-def hough_detector(image_path, padding_r=0):
-    success = False
-    image = cv2.imread(image_path) if isinstance(image_path, str) else cv2.cvtColor(image_path, cv2.COLOR_BGR2RGB)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(image, 11)
-    ret, _ = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    circles = cv2.HoughCircles(
-        gray,
-        cv2.HOUGH_GRADIENT,
-        1,
-        50,
-        param1=ret,
-        param2=30,
-        minRadius=20,
-        maxRadius=100,
-    )
-    try:
-        circles = circles[0, :, :]
-        circles = np.int16(np.array(circles))
-        for i in circles[:]:
-            image = image[i[1] - i[2] - padding_r:i[1] + i[2] + padding_r, i[0] - i[2] - padding_r:i[0] + i[2] + padding_r]
-            radius = i[2]
-        success = True
-        return image, radius, success
-    except:
-        image[:] = 255
-        success = False
-        return image, image.shape[0], success
 
+def containment_percentage(box_a, box_b):
+    xa1, ya1, xa2, ya2 = box_a
+    xb1, yb1, xb2, yb2 = box_b
 
+    xi1 = max(xa1, xb1)
+    yi1 = max(ya1, yb1)
+    xi2 = min(xa2, xb2)
+    yi2 = min(ya2, yb2)
 
-def find_pupil(image):
-    if isinstance(image, str):
-        image = cv2.imread(image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    inter_width = max(0, xi2 - xi1)
+    inter_height = max(0, yi2 - yi1)
+    inter_area = inter_width * inter_height
 
-    image_roi, radius, success = hough_detector(image, -5)
-    return image_roi, radius, success
+    area_a = (xa2 - xa1) * (ya2 - ya1)
+
+    if area_a == 0:
+        return 0.0
+    return inter_area / area_a
 
 
 
 def normalize_eye(image, model, output_size=(60, 360)):
-    results = detect(model, image, "cuda" if torch.cuda.is_available() else "cpu")
-    if len(results) == 0:
-        raise ValueError("No eye detected in the image.")
-    try:
-        box = results.boxes.xyxy.cpu().tolist()[0]
-    except:
-        print("No eye detected in the image. Returning a blank image.")
-        return np.zeros((output_size[0], output_size[1], 3), dtype=np.uint8)
+    detections = {
+        0: [], # Pupils
+        1: [], # Iris
+    }
+    results = detect(model, image)
 
+    for result in results:
+        class_id = int(result.boxes.cls.cpu().numpy()[0])
+        if class_id != 1:
+            continue
+        box = result.boxes.xyxy[0].cpu().numpy()
+        score = result.boxes.conf.cpu().numpy()[0]
+
+        if len(detections[1]) == 1:
+            actual_score = detections[1][0][2]
+            if score > actual_score:
+                detections[1][0] = (box, score)
+        else:
+            detections[1].append((box, score))
+            
+
+    for result in results:
+        class_id = int(result.boxes.cls.cpu().numpy()[0])
+        if class_id != 0:
+            continue
+        box = result.boxes.xyxy[0].cpu().numpy()
+        score = result.boxes.conf.cpu().numpy()[0]
+        
+        if len(detections[0]) == 1:
+            iris_box, _ = detections[1][0]
+            iou = containment_percentage(box, iris_box)
+            if iou >= 0.8:
+                actual_score = detections[0][0][1]
+                if score > actual_score:
+                    detections[0][0] = (box, score)
+        else:
+            iris_box, _ = detections[1][0]
+            iou = containment_percentage(box, iris_box)
+            if iou >= 0.8:
+                detections[0].append((box, score))
+
+    iris_box = detections[1][0][0] if len(detections[1]) > 0 else None
+    pupil_box = detections[0][0][0] if len(detections[0]) > 0 else None
+    if iris_box is None or pupil_box is None:
+        print("No iris or pupil detected")
+        return None
     image_crop = image.crop(
         (
-            box[0],
-            box[1],
-            box[2],
-            box[3],
+            int(iris_box[0]),
+            int(iris_box[1]),
+            int(iris_box[2]),
+            int(iris_box[3])
         )
     )
     image_crop = np.array(image_crop)
-    image_r = image_crop.shape[0] // 2
-
-    _, pupil_r, success = find_pupil(image_crop)
-    if not success:
-        pupil_r = image_r // 2
+    image_r = image_crop.shape[0] / 2
+    pupil_r = int((pupil_box[2] - pupil_box[0]) / 2)
 
     normalized_eye = daugman_normalization(image_crop, output_size[0], output_size[1], pupil_r, image_r)
     return normalized_eye
-
