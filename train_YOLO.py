4666855e62a4371f310234747fcfb072e5c0ca7d
JustCati <ale.catinello@gmail.com>
2025-05-26 13:59:51 +0200

    feat: add data augmentation and processing functions for image files

--- train_YOLO.py
+++ train_YOLO.py
@@ -1,6 +1,12 @@
 import os
 import argparse
 
+import multiprocessing
+from PIL import Image
+from tqdm import tqdm
+from torchvision.transforms import v2 as T
+from concurrent.futures import ThreadPoolExecutor
+
 from src.utils.utils import get_device
 from src.models.yolo import getYOLO, train as yolo_train
 from src.utils.dataset_utils.yolo import convert_ann_to_yolo
@@ -18,6 +24,36 @@ CONVERT_CLASS = {
     5 : 0,
     6 : 0 # Both eyes are the same class
 }
+
+
+def process_file(in_path, out_path):
+    pipeline = T.Compose([
+        T.Grayscale(num_output_channels=3),
+    ])
+    
+    img = Image.open(in_path)
+    try:
+        updated_img = pipeline(img)
+    except Exception as e:
+        print(f"Error processing {in_path}: {e}")
+        return
+
+    if not os.path.exists(os.path.dirname(out_path)):
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    updated_img.save(out_path)
+
+
+def augment_data(dataset_path, out_path):
+    files_to_process = []
+    for folder in os.listdir(dataset_path):
+        folder_path = os.path.join(dataset_path, folder)
+        for file in os.listdir(folder_path):
+            if file.endswith('.jpg') or file.endswith('.png'):
+                files_to_process.append((os.path.join(folder_path, file), os.path.join(out_path, folder, file)))
+
+    print(f"Found {len(files_to_process)} files to process in {dataset_path}")
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+        list(tqdm(executor.map(lambda x: process_file(*x), files_to_process), total=len(files_to_process)))
 
 
 
@@ -54,6 +90,10 @@ def main(args):
     print(f"Destination annotations count: {dst_ann_count}")
     if src_ann_count != dst_ann_count:
         convert_ann_to_yolo(portrait_ann, yolo_portrait_ann, CLASSESS, CONVERT_CLASS)
+
+    if not os.path.exists(os.path.join(portrait_dataset_path, 'images')):
+        augment_data(dataset_path=os.path.join(portrait_dataset_path, 'images_raw'),
+                      out_path=os.path.join(portrait_dataset_path, 'images'))
 
     #* Format yaml file
     if not os.path.exists(easy_portrait_yaml_path):
