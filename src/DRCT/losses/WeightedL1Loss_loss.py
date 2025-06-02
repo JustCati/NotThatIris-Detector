822733353ea01b6010533f1d2eb4ce04cb54c2a6
JustCati <ale.catinello@gmail.com>
2025-06-02 12:39:20 +0200

    fix: update WeightedL1Loss to support configurable reduction methods

--- src/DRCT/losses/WeightedL1Loss_loss.py
+++ src/DRCT/losses/WeightedL1Loss_loss.py
@@ -6,9 +6,17 @@ from basicsr.utils.registry import LOSS_REGISTRY
 
 @LOSS_REGISTRY.register()
 class WeightedL1Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
+        self.reduction = reduction
         super(WeightedL1Loss, self).__init__()
 
     def forward(self, input, target):
         mask = target != 0
-        return torch.abs(input - target)[mask].mean()
+        value = torch.abs(input - target)[mask]
+        
+        if self.reduction == 'mean':
+            return value.mean()
+        elif self.reduction == 'sum':
+            return value.sum()
+        else:
+            raise ValueError(f"Unsupported reduction: {self.reduction}")
